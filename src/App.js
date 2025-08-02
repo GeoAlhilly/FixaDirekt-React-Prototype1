bbce0bc2c421831a40ef173393d7a2c0f82c5c94
GeoAlhilly <georgealhilly@gmail.com>
2025-08-02 22:49:30 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
+      <h1>Välkommen till FixaDirekt!</h1>
+      <p>Det här är en fungerande React-prototyp.</p>
+    </div>
+  );
+}
+
+export default App;
